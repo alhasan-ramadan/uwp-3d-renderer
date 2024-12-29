f77165e161f896d8979f384eaa9ea6d86193a789
Hasan <152815143+hadespreyz@users.noreply.github.com>
2024-12-29 13:55:03 +0100

    Added basic classes

--- SoftEngine/MainWindow.xaml.cs
+++ SoftEngine/MainWindow.xaml.cs
@@ -1,28 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using SharpDX;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace SoftEngine
 {
-    /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public sealed partial class MainPage : Page
     {
-        public MainWindow()
+        private Device _device;
+        private readonly Camera _camera = new Camera();
+        private Mesh[] _meshes;
+
+        public MainPage()
         {
             InitializeComponent();
+            Loaded += Page_Loaded;
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Choose the back buffer resolution here
+            WriteableBitmap bmp = new WriteableBitmap(640, 480);
+
+            // Our Image XAML control
+            frontBuffer.Source = bmp;
+
+            _device = new Device(bmp);
+            _meshes = await _device.LoadJsonFileAsync("monkey.babylon");
+            _camera.Position = new Vector3(0, 0, 10.0f);
+            _camera.Target = Vector3.Zero;
+
+            CompositionTarget.Rendering += CompositionTarget_Rendering;
+        }
+
+        private void CompositionTarget_Rendering(object sender, object e)
+        {
+            _device.Clear(0, 0, 0, 255);
+
+            foreach (var mesh in _meshes)
+            {
+                // rotating slightly the meshes during each frame rendered
+                mesh.Rotation = new Vector3(mesh.Rotation.X, mesh.Rotation.Y + 0.01f, mesh.Rotation.Z);
+            }
+
+            // Doing the various matrix operations
+            _device.Render(_camera, _meshes);
+            // Flushing the back buffer into the front buffer
+            _device.Present();
         }
     }
 }
---
+++ SoftEngine/Mesh.cs
@@ -0,0 +1,22 @@
+﻿using SharpDX;
+
+namespace SoftEngine
+{
+    public class Mesh
+    {
+        public string Name { get; private set; }
+        public Vertex[] Vertices { get; private set; }
+        public Face[] Faces { get; private set; }
+        public Vector3 Position { get; set; }
+        public Vector3 Rotation { get; set; }
+
+        public Texture Texture { get; set; }
+
+        public Mesh(string name, int verticesCount, int facesCount)
+        {
+            Name = name;
+            Faces = new Face[facesCount];
+            Vertices = new Vertex[verticesCount];
+        }
+    }
+}
